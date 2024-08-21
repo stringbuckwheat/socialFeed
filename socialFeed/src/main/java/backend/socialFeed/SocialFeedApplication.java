88c0906dbd884a791795a76c6662095fba0c7e7b
lielocks <kari300084@gmail.com>
2024-08-21 12:25:33 +0900

    [Feat] 프로젝트 초기 설정

---
+++ socialFeed/src/main/java/backend/socialFeed/SocialFeedApplication.java
@@ -0,0 +1,13 @@
+package backend.socialFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialFeedApplication.class, args);
+	}
+
+}
---
+++ socialFeed/src/test/java/backend/socialFeed/SocialFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.socialFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

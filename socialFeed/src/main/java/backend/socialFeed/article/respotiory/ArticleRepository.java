72e4ca6fcdaa66fe6a88390334fc392e84718a0d
yujin <uj0791navercom@skuniv.ac.kr>
2024-08-25 21:19:15 +0900

    [Fix] repository 충돌

--- socialFeed/src/main/java/backend/socialFeed/article/respotiory/ArticleRepository.java
+++ socialFeed/src/main/java/backend/socialFeed/article/respotiory/ArticleRepository.java
@@ -1,9 +1,25 @@
 package backend.socialFeed.article.respotiory;
 
+import backend.socialFeed.article.ArticleType;
 import backend.socialFeed.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
 public interface ArticleRepository extends JpaRepository<Article, String> {
+
+    @Query("SELECT a FROM Article a LEFT JOIN a.hashtags h " +
+            "WHERE (:hashtag IS NULL OR h.name = :hashtag) " +
+            "AND (:type IS NULL OR a.type = :type) " +
+            "AND ((:searchBy = 'title' AND a.title LIKE %:search%) " +
+            "OR (:searchBy = 'content' AND a.content LIKE %:search%) " +
+            "OR (:searchBy = 'title,content' AND (a.title LIKE %:search% OR a.content LIKE %:search%)))")
+    Page<Article> searchArticles(@Param("hashtag") String hashtag,
+                                 @Param("type") ArticleType type,
+                                 @Param("searchBy") String searchBy,
+                                 @Param("search") String search,
+                                 Pageable pageable);
+
 }

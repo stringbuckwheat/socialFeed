a89d8725b9d1dda8c3b29f68cdc4ad1e1a53be48
stringbuckwheat <stringbuckwheat@gmail.com>
2024-08-26 16:52:21 +0900

    [Feat] 요청 유효성 검사 로직 수정 및 @EqualsAndHashCode 어노테이션 추가

--- socialFeed/src/main/java/backend/socialFeed/statistic/dto/StatisticResponse.java
+++ socialFeed/src/main/java/backend/socialFeed/statistic/dto/StatisticResponse.java
@@ -1,11 +1,13 @@
 package backend.socialFeed.statistic.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @Getter
+@EqualsAndHashCode
 public class StatisticResponse {
     private String date; // 일별/시간대별 통계 결과를 위해 String으로 포맷
     private Integer count;
